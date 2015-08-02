d4b688912e89c5159d3e7c2eb13a2bcdc883683e
Adam <adam.laycock@gmail.com>
2015-08-01 21:12:51 -0600

    Adding testing

---
+++ README.md
@@ -0,0 +1,17 @@
+# ExpressToDo #
+Simple todo app using Node.js and Express that connects to a RESTful CRUD
+interface. A test CRUD interface can be found at
+[TeamTrumpet/simplestore](https://github.com/TeamTrumpet/simplestore).
+
+## Installation ##
+
+To clone, install, and run, execute the following commands.
+
+    git clone https://github.com/alaycock/ExpressToDo
+    set -x SERVER_URL server_url
+    set -x SERVER_USERNAME username
+    set -x SERVER_PASSWORD password
+    npm install
+    npm start
+
+Replace `server_url`, `username` and `password` with your server details.
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
-var todo       = require('./todo');
+var request    = require('request');
+var ToDo       = require('./lib/todo');
+var Sender     = require('./lib/requestSend');
 var app        = express();
+var router     = express.Router();
 
 // Helps parse the body of POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,8 +13,15 @@ app.use(bodyParser.json());
 // Serve static files from public directory
 app.get('/', express.static('public'));
 
-// For any API paths, route them to the API
-app.use('/todo', todo);
+// For any /todo paths, route them to the todo API
+var todo = new ToDo(new Sender(request));
+router.get('/', todo.getAll)
+      .post('/', todo.post)
+      .get('/:todo_id', todo.get)
+      .put('/:todo_id', todo.put)
+      .delete('/:todo_id', todo.delete);
+
+app.use('/todo', router);
 
 // Run the server
 var server = app.listen(3000, function () {
